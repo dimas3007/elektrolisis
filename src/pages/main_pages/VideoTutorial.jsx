392e842e9f13d5787833b61e82fdf77ba7c60da1
Dimas Setiaji <dimassetiaji30@gmail.com>
2023-09-25 17:44:51 +0700

    video content

--- src/pages/main_pages/VideoTutorial.jsx
+++ src/pages/main_pages/VideoTutorial.jsx
@@ -1,5 +1,10 @@
 import iconTentang from "../../assets/img/icon/tentang_kami.png";
-import { BsFillCalendarWeekFill } from "react-icons/bs";
+import {
+  BsFillCalendarWeekFill,
+  BsFillPlayFill,
+  BsCollectionPlayFill,
+  BsCollectionPlay,
+} from "react-icons/bs";
 import { AiFillEye, AiFillHeart, AiFillPlayCircle } from "react-icons/ai";
 import { BiSolidCloudDownload } from "react-icons/bi";
 import HeadingContent from "../../layouts/components/HeadingContent";
@@ -58,18 +63,64 @@ const VideoTutorial = () => {
             </div>
           </div>
         </div>
-        <div className="content-description">
-          <h2>
-            Bagaimana Cara Menggunakan <i>Excel</i> Elektrolisis Interaktif?
-          </h2>
-          <p>
-            Excel elektrolisis berisi penggambaran dari proses sel elektrolisis
-            menggunakan media pembelajaran digital excel. Penjelasan cara
-            penggunaan media interaktif ini akan dijelaskan dengan menggunakan
-            video tutorial di atas. Kalian dapat menggunakan shortcut di bawah
-            untuk langsung menonton topik-topik yang belum dipahami cara
-            penggunaannya <FaRegFaceSmileWink />
-          </p>
+        <div className="content-wrapp-grid">
+          <div className="content-description">
+            <h2>
+              Bagaimana Cara Menggunakan <i>Excel</i> Elektrolisis Interaktif?
+            </h2>
+            <p>
+              Excel elektrolisis berisi penggambaran dari proses sel
+              elektrolisis menggunakan media pembelajaran digital excel.
+              Penjelasan cara penggunaan media interaktif ini akan dijelaskan
+              dengan menggunakan video tutorial di atas. Kalian dapat
+              menggunakan shortcut di bawah untuk langsung menonton topik-topik
+              yang belum dipahami cara penggunaannya <FaRegFaceSmileWink />
+            </p>
+          </div>
+          <div className="content-part">
+            <div className="part-header">
+              <h2>Konten Video</h2>
+              <BsCollectionPlay />
+            </div>
+            <p>
+              klik pada salah satu konten video dibawah ini untuk melihat video
+              materi terkait.
+            </p>
+            <div className="part-wrapper">
+              <div className="part-item">
+                <BsFillPlayFill />
+                <h4>Standar Kompetensi</h4>
+              </div>
+              <div className="part-item">
+                <BsFillPlayFill />
+                <h4>Kompetensi Dasar</h4>
+              </div>
+              <div className="part-item">
+                <BsFillPlayFill />
+                <h4>Indikator</h4>
+              </div>
+              <div className="part-item">
+                <BsFillPlayFill />
+                <h4>Tujuan Eksperimen</h4>
+              </div>
+              <div className="part-item">
+                <BsFillPlayFill />
+                <h4>Alat dan Bahan</h4>
+              </div>
+              <div className="part-item">
+                <BsFillPlayFill />
+                <h4>Video</h4>
+              </div>
+              <div className="part-item">
+                <BsFillPlayFill />
+                <h4>Elektrolisis Interaktif</h4>
+              </div>
+              <div className="part-item">
+                <BsFillPlayFill />
+                <h4>Stoikiometri</h4>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
