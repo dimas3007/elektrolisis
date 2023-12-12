5ac0fe7ff0a88472ae4e5f06daad92b6331a8a4b
Dimas Setiaji <dimassetiaji30@gmail.com>
2023-12-12 08:46:41 +0700

    Update SoalLatihan.jsx

--- src/data/SoalLatihan.jsx
+++ src/data/SoalLatihan.jsx
@@ -4,7 +4,7 @@ const SoalLatihan = {
   soalData: [
     {
       question:
-        "Pada elektrolisis lelehan MgCl<sub>2</sub> menggunakan elektroda grafit, pada katoda dan anoda berturut-turut dibebaskan...",
+        "Jika terjadi elektrolisis dari lelehan MgCl<sub>2</sub> menggunakan elektroda grafit, maka pada katoda dan anoda berturut-turut dibebaskan...",
       options: [
         "H<sub>2</sub> dan Cl<sub>2</sub>",
         "H<sub>2</sub> dan O<sub>2</sub>",
